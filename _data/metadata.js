9997f6094dd69d72c54cc5dd7073b524f9d25e4f
Saifullah Qaumi <S4ifazeez@gmail.com>
2024-08-21 13:10:20 +0100

    I edited the _data/metadata.js with my blog’s information.

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
 	title: "Eleventy Base Blog v8",
-	url: "https://example.com/",
+	url: "https://just-saif.netlify.app/",
 	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	description: "I am writing about my experiences as a young developer.",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
+		name: "Saifullah",
+		email: "S4ifazeez@gmail.com",
+		url: "www.linkedin.com/in/saifullah-qaumi-8131452b5"
 	}
 }
